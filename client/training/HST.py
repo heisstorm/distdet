b8c0618de9e7a984632f8c03127fa8ed30ea8af5
heisstorm <shijielu@asu.edu>
2024-03-03 01:44:00 -0700

    HST 新增NLP支持

--- client/training/HST.py
+++ client/training/HST.py
@@ -7,6 +7,9 @@ import sys
 sys.path.append("../../")
 from decimal import Decimal
 from client.common.mask import mask_ip
+import spacy
+from collections import Counter
+import csv
 
 # sysdig -p"%evt.num %evt.rawtime.s.%evt.rawtime.ns %evt.cpu %proc.name (%proc.pid) %evt.dir %evt.type cwd=%proc.cwd %evt.args latency=%evt.latency" -s 200 evt.type!=switch and proc.name!=sysdig > system_log.txt
 # sysdig -p"%evt.num %evt.rawtime.s.%evt.rawtime.ns %evt.cpu %proc.name (%proc.pid) %evt.dir %evt.type cwd=%proc.cwd %evt.args latency=%evt.latency" -s 200 evt.type!=switch and proc.name!=sysdig -A -w system_log.txt -G 60
@@ -25,6 +28,10 @@ model_n_connect = set()
 # path has only 1, but name can be even more, so use key to find path is faster
 procname_path = {}
 
+# use Spicy NLP to merge the similar sink attributes and add the frequency, execve and 4 file models
+# will greatly reduce the model size and must set the NLP to be true when detection.
+use_NLP_merge = True
+NLP_threshold = 0.66
 
 def process_log_line(line):
     parts = line.split()
@@ -202,17 +209,107 @@ def merge_or_create_csv(file_name_client, pandas_dataframe):
     else:
         pandas_dataframe.to_csv(file_name_client, index=False)
 
-
 def post_processing_pandas():
-    merge_or_create_csv('model_p_execve_c.csv',
-                        pd.DataFrame(model_p_execve, columns=["source", "sink", "freq"]))
-    merge_or_create_csv('model_f_create_c.csv', pd.DataFrame(model_f_create, columns=["source", "sink", "freq"]))
-    merge_or_create_csv('model_f_modify_c.csv', pd.DataFrame(model_f_modify, columns=["source", "sink", "freq"]))
-    merge_or_create_csv('model_f_delete_c.csv', pd.DataFrame(model_f_delete, columns=["source", "sink", "freq"]))
-    merge_or_create_csv('model_f_rename_c.csv', pd.DataFrame(model_f_rename, columns=["source", "sink", "freq"]))
-    merge_or_create_csv('model_n_listen_c.csv', pd.DataFrame(model_n_listen, columns=["source", "sink", "freq"]))
-    merge_or_create_csv('model_n_connect_c.csv', pd.DataFrame(model_n_connect, columns=["source", "sink", "freq"]))
+    model_sets = {
+        "model_p_execve": model_p_execve,
+        "model_f_create": model_f_create,
+        "model_f_modify": model_f_modify,
+        "model_f_delete": model_f_delete,
+        "model_f_rename": model_f_rename,
+        "model_n_listen": model_n_listen,
+        "model_n_connect": model_n_connect
+    }
+    for var_name, var in model_sets.items():
+        file_name = '%s_c.csv' % var_name
+        merge_or_create_csv(file_name, pd.DataFrame(var, columns=["source", "sink", "freq"]))
+        if use_NLP_merge:
+            if var_name not in ["model_n_listen", "model_n_connect"]:
+                NLP_merge(file_name)
 
+
+def NLP_merge(file_name):
+    log = read_csv_to_2d_dict(file_name)
+    nlp = spacy.blank("en")
+    for key in log.keys():
+        log[key] = merge_similar_logs_with_freq(log[key], nlp, threshold=NLP_threshold)
+    save_2d_dict_to_csv(log, file_name)
+
+def merge_similar_logs_with_freq(logs_dict, nlp_model, threshold):
+    logs = list(logs_dict.keys())
+    frequencies = list(logs_dict.values())
+
+    i = 0
+    while i < len(logs) - 1:
+        tokenized_log1 = nlp_model(logs[i])
+        log1_tokens = [token.text for token in tokenized_log1]
+
+        j = i + 1
+        while j < len(logs):
+            tokenized_log2 = nlp_model(logs[j])
+            log2_tokens = [token.text for token in tokenized_log2]
+
+            similarity = calculate_similarity(log1_tokens, log2_tokens)
+            if similarity >= threshold:
+                # Merge log1 and log2 by masking differences in log1
+                merged_log = ['*' if token not in log2_tokens else token for token in log1_tokens]
+                merged_log_str = ''.join(merged_log)
+
+                # Update log entry with merged log and add frequencies
+                logs[i] = merged_log_str
+                frequencies[i] += frequencies[j]
+
+                # Remove log2 from consideration
+                logs.pop(j)
+                frequencies.pop(j)
+            else:
+                # Move to next log if current pair is not similar
+                j += 1
+        i += 1
+
+    # Reconstruct the dictionary from the merged logs and their frequencies
+    merged_logs_dict = {log: freq for log, freq in zip(logs, frequencies)}
+    return merged_logs_dict
+
+
+def calculate_similarity(log1_tokens, log2_tokens):
+    log1_counter = Counter(log1_tokens)
+    log2_counter = Counter(log2_tokens)
+    common_tokens = log1_counter & log2_counter
+    total_common = sum(common_tokens.values())
+    # deal with not common tokens
+    # Counter({'gpghome.7x2RItn9yt': 2})
+    # non_common_tokens_log1 = log1_counter - log2_counter
+    # Counter({'gpghome.8OpL2Yibqy': 1, 'gpghome.8OpL2Yibq': 1})
+    # non_common_tokens_log2 = log2_counter - log1_counter
+    similarity = total_common / max(len(log1_tokens), len(log2_tokens))
+    return similarity
+
+
+def read_csv_to_2d_dict(filename):
+    with open(filename, mode='r', encoding='utf-8') as file:
+        reader = csv.DictReader(file)
+        log_dict = {}
+        for row in reader:
+            source = row['source']
+            sink = row['sink']
+            frequency = int(row['freq'])  # Convert frequency to integer
+
+            if source not in log_dict:
+                log_dict[source] = {}
+            log_dict[source][sink] = frequency
+    return log_dict
+
+
+def save_2d_dict_to_csv(log_frequencies, filename):
+    with open(filename, mode='w', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        # Write the header
+        writer.writerow(['source', 'sink', 'freq'])
+
+        # Write the data
+        for source, sinks in log_frequencies.items():
+            for sink, frequency in sinks.items():
+                writer.writerow([source, sink, frequency])
 
 def perf_info_calculation():
     execve = os.path.getsize('model_p_execve_c.csv') / 1024
